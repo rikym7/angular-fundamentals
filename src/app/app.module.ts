b6320900d21915d03752e73ef7dbb7473f850f48
Rick Myers <rikym7@gmail.com>
2018-12-02 17:04:40 -0500

    Added CreateEvent component and routing from code for cancel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service' 
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './routes'
+import { CreateEventComponent } from './events/create-event.component'
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { appRoutes } from './routes'
     EventsListComponent,
     EventThumbnailComponent,
     EventDetailsComponent,
+    CreateEventComponent,
     NavBarComponent
   ],
   imports: [
