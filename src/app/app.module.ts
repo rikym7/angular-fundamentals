a9a838b3f8b1c309ebb60bc5c10b6405fb94501b
Rick Myers <rikym7@gmail.com>
2018-12-16 12:09:21 -0500

    Mangaing exports with Barreling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ComponentFactoryResolver } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
+
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent} from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.componet';
+//import { EventsListComponent} from './events/events-list.component';
+//import { EventThumbnailComponent } from './events/event-thumbnail.componet';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
+//import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service' ;
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
+//import { EventDetailsComponent } from './events/event-details/event-details.component';
+//import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+//import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { appRoutes } from './routes';
-import { EventListResolver } from './events/events-list-resolver.service'
+//import { EventListResolver } from './events/events-list-resolver.service'
 
 
 @NgModule({
---
+++ src/app/events/index.ts
@@ -0,0 +1,9 @@
+//Creating a Barrel to export 
+
+export * from './create-event.component'
+export * from './event-thumbnail.componet'
+export * from './events-list-resolver.service'
+export * from './events-list.component'
+export * from './shared/index'
+export * from './event-details/index'
+
