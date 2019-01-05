782e45d1da3f88b913946319e4b010571cb2ca8c
Rick Myers <rikym7@gmail.com>
2019-01-05 11:57:33 -0500

    Fixed a declaration problem on the login compt - must define bind vars!

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core'
 })
 
 export class LoginComponent {
-
+    userName: string
+    password: string
     login(formValues) {
         console.log(formValues)
     }
