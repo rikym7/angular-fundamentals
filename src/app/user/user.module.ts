9f84fb29d868b93965f4535b47b4968797e4b609
Rick Myers <rikym7@gmail.com>
2019-01-05 11:50:54 -0500

    Added basic Form passing of values to component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,16 +2,20 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 import { userRoutes } from './user.routes'
 import { ProfileComponent } from './profile.component'
+import { LoginComponent } from './login.component'
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule.forChild(userRoutes) //for the app module it was .forRoot
     ],
     declarations: [
-        ProfileComponent
+        ProfileComponent,
+        LoginComponent
     ],
     providers: []
 })
