1c40abfa18e2d1a409c95c180c18a37d74c5a4be
Rick Myers <rikym7@gmail.com>
2019-01-20 14:00:45 -0500

    can change profile using reactive form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup } from '@angular/forms';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router'
 
 @Component({
   templateUrl: './profile.component.html'
@@ -9,7 +10,7 @@ import { AuthService } from './auth.service';
 export class ProfileComponent implements OnInit {
   profileForm:FormGroup
 
-  constructor(private authService:AuthService) {
+  constructor(private authService:AuthService, private router:Router) {
 
   }
 
@@ -20,6 +21,16 @@ export class ProfileComponent implements OnInit {
         firstName: firstName,
         lastName: lastName
     })
+
+  }
+
+  cancel() {
+    this.router.navigate(['events'])
+  }
+
+  saveProfile(formValues) {
+    this.authService.updateCurrentUser(formValues.firstName,formValues.lastName)
+    this.router.navigate(['events'])
 
   }
 
